e4ad788cd2e936023a9ef8a93f856e3e809a4dd8
SOPHEA.SOPHORN <SOPHEA.SOPHORN@PNC.LOCAL>
2026-06-11 09:41:13 +0700

    adding api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\Api\CategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
